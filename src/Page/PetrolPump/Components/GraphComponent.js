2f59dd8707f266cbbab5bf426b962e58caf3a1dd
Sreeraksha <raksharamesh1998@gmail.com>
2023-09-16 21:33:14 +0530

    changing color to petrol project

--- src/Page/PetrolPump/Components/GraphComponent.js
+++ src/Page/PetrolPump/Components/GraphComponent.js
@@ -7,7 +7,7 @@ const GraphComponent = () => {
             <tr>
                 <td></td>
                 <td>
-                    <div className='grid grid-cols-2 nodes text-center' style={{background:"#FBF0B2"}}>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
                         <div className='col-span-2 eachCell nodeName'>A</div>
                         <div className='eachCell'>d</div>
                         <div className='eachCell'>f</div>
@@ -25,8 +25,19 @@ const GraphComponent = () => {
             </tr>
 
             <tr>
+
                 <td>
-                    <div className='grid grid-cols-2 nodes text-center' style={{background:"#D8B4F8"}}>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
+                        <div className='col-span-2 eachCell nodeName'>F</div>
+                        <div className='eachCell'>d</div>
+                        <div className='eachCell'>f</div>
+                        <div className='eachCell'>60</div>
+                        <div className='eachCell'>40</div>
+                    </div>
+                </td>
+                <td></td>
+                <td>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
                         <div className='col-span-2 eachCell nodeName'>B</div>
                         <div className='eachCell'>d</div>
                         <div className='eachCell'>f</div>
@@ -34,9 +45,27 @@ const GraphComponent = () => {
                         <div className='eachCell'>20</div>
                     </div>
                 </td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
+                        <div className='col-span-2 eachCell nodeName'>E</div>
+                        <div className='eachCell'>d</div>
+                        <div className='eachCell'>f</div>
+                        <div className='eachCell'>20</div>
+                        <div className='eachCell'>50</div>
+                    </div>
+                </td>
                 <td></td>
                 <td>
-                    <div className='grid grid-cols-2 nodes text-center'>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
                         <div className='col-span-2 eachCell nodeName'>C</div>
                         <div className='eachCell'>d</div>
                         <div className='eachCell'>f</div>
@@ -53,42 +82,14 @@ const GraphComponent = () => {
             </tr>
 
             <tr>
+                <td></td>
                 <td>
-                    <div className='grid grid-cols-2 nodes text-center'>
+                    <div className='grid grid-cols-2 nodes text-center' style={{ background: "#333652", color:'white'}}>
                         <div className='col-span-2 eachCell nodeName'>D</div>
                         <div className='eachCell'>d</div>
                         <div className='eachCell'>f</div>
                         <div className='eachCell'>30</div>
                         <div className='eachCell'>30</div>
-                    </div>
-                </td>
-                <td></td>
-                <td>
-                    <div className='grid grid-cols-2 nodes text-center'>
-                        <div className='col-span-2 eachCell nodeName'>E</div>
-                        <div className='eachCell'>d</div>
-                        <div className='eachCell'>f</div>
-                        <div className='eachCell'>20</div>
-                        <div className='eachCell'>50</div>
-                    </div>
-                </td>
-            </tr>
-
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-
-            <tr>
-                <td></td>
-                <td>
-                    <div className='grid grid-cols-2 nodes text-center'>
-                        <div className='col-span-2 eachCell nodeName'>F</div>
-                        <div className='eachCell'>d</div>
-                        <div className='eachCell'>f</div>
-                        <div className='eachCell'>60</div>
-                        <div className='eachCell'>40</div>
                     </div>
                 </td>
                 <td></td>
